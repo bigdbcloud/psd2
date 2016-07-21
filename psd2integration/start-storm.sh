cb1bfd5982bcc1a577198e89d2404eaadbe03378
root <root@inmbz2239.in.dst.ibm.com>
2016-07-21 21:31:58 +0530

    refactoring changes

--- psd2integration/start-storm.sh
+++ psd2integration/start-storm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export STORM_HOME=/usr/local/apache-storm-1.0.1/
+export STORM_HOME=/usr/local/digiplat/software/apache-storm-1.0.1/
 
 echo $STORM_HOME
 
 $STORM_HOME/bin/storm jar /media/sf_psd2/psd2/psd2integration/target/psd2integration-1.0.1-jar-with-dependencies.jar com.ibm.psd2.integration.App \
---zookeeper.host 172.30.77.154:2181 \
+--zookeeper.host inmbz2239.in.dst.ibm.com:2181 \
 --kafka.topic psd2payments \
---mongodb.host mongodb://172.30.46.38:27017 \
+--mongodb.host mongodb://inmbz2239.in.dst.ibm.com:27017 \
 --job.submission.type storm  \
 --psd2api.db psd2api \
 --payments.collection payments \
