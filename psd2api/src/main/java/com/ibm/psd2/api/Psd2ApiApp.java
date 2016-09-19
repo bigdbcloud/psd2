78591ea563c790fc3eba6daf32854ce07258a63c
tambre <tambre@in.ibm.com>
2016-09-19 23:48:22 +0530

    fixed runtime issues

--- psd2api/src/main/java/com/ibm/psd2/api/Psd2ApiApp.java
+++ psd2api/src/main/java/com/ibm/psd2/api/Psd2ApiApp.java
@@ -8,13 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.ibm.psd2.api.subscription.db", "com.ibm.psd2.api.subscription.service",
-		"com.ibm.psd2.api.user.db", "com.ibm.psd2.api.user.service",
-		"com.ibm.psd2.api.admin.controller", "com.ibm.psd2.api.aip.controller", "com.ibm.psd2.api.aip.db",
-		"com.ibm.psd2.api.aip.services", "com.ibm.psd2.api.integration", "com.ibm.psd2.api.pisp.controller",
-		"com.ibm.psd2.api.pisp.db", "com.ibm.psd2.api.pisp.service", "com.ibm.psd2.api.txns.service",
-		"com.ibm.psd2.api.security", "com.ibm.psd2.api.subscription.controller", "com.ibm.psd2.api.swagger",
-		"com.ibm.psd2.api.utils" })
+@ComponentScan(basePackages = {"com.ibm.psd2.api"})
 public class Psd2ApiApp
 {
 	@RequestMapping("/user")
