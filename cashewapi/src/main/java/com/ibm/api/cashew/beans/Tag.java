447fbe570f8ca41939286d15e7006f293670fd33
Nisha Bhagdev <nisha@nisha-VirtualBox>
2016-09-22 21:10:16 +0530

    Added missing files

---
+++ cashewapi/src/main/java/com/ibm/api/cashew/beans/Tag.java
@@ -0,0 +1,25 @@
+package com.ibm.api.cashew.beans;
+
+public class Tag {
+
+	private String name;
+	private String spentLimit;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	public String getSpentLimit() {
+		return spentLimit;
+	}
+
+	public void setSpentLimit(String spentLimit) {
+		this.spentLimit = spentLimit;
+	}
+
+}
