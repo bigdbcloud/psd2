c52c45267fef857d687e765d22b04b7d7cc3606d
tambre <tambre@in.ibm.com>
2016-09-22 21:44:37 +0530

    for creating consolidated containers

--- cashewstartup.sh
+++ cashewstartup.sh
@@ -10,6 +10,7 @@ chmod -R 777 /logs
 chmod -R 777 /data/configdb
 chmod -R 777 /usr/share/elasticsearch/data
 chmod -R 777 /
+chmod -R 777 /usr/local/cashew/elastic.sh
 
 echo $MOUNTPATH
 
@@ -19,11 +20,11 @@ ls -al /data/configdb
 
 set -e
 
-
 #echo "Starting mongodb logs available at: /logs/mongodb.log"
 #exec mongod --fork --logpath /logs/mongodb.log
 #echo "API Server logs available at: /logs"
 #exec java -jar /usr/local/mcabuddyapp/mcabuddyapi-beta.jar
 
-elasticsearch -Des.insecure.allow.root=true & mongod --logpath /logs/mongodb.log & java -jar /usr/local/psd2/cashewapi-0.0.1-SNAPSHOT.jar & java -jar /usr/local/psd2/bigoauth2server-beta.jar
+#elasticsearch -Des.insecure.allow.root=true & mongod --logpath /logs/mongodb.log & java -jar /usr/local/cashew/cashewapi-0.0.1-SNAPSHOT.jar & java -jar /usr/local/cashew/bigoauth2server-beta.jar
+/usr/local/cashew/elastic.sh elasticsearch & mongod --logpath /logs/mongodb.log & java -jar /usr/local/cashew/cashewapi-0.0.1-SNAPSHOT.jar & java -jar /usr/local/cashew/bigoauth2server-beta.jar
 
