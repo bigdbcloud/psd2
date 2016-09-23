94ff596a21df377ccf79aa54c190fd60d4e39cd7
Nisha Bhagdev <nisha@nisha-VirtualBox>
2016-09-23 18:55:45 +0530

    Added API's for barclays account payee

--- cashewapi/src/main/java/com/ibm/api/cashew/services/barclays/BarclaysService.java
+++ cashewapi/src/main/java/com/ibm/api/cashew/services/barclays/BarclaysService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import com.ibm.api.cashew.beans.UserAccount;
 import com.ibm.psd2.datamodel.aip.BankAccountDetailsView;
 import com.ibm.psd2.datamodel.aip.Transaction;
+import com.ibm.psd2.datamodel.pisp.CounterParty;
 import com.ibm.psd2.datamodel.subscription.SubscriptionRequest;
 
 public interface BarclaysService {
@@ -15,8 +16,8 @@ public interface BarclaysService {
 
 	SubscriptionRequest subscribe(SubscriptionRequest subscriptionRequest);
 
-	Map<String,BankAccountDetailsView> getAccountInformation(UserAccount ua) throws URISyntaxException;
+	Map<String, BankAccountDetailsView> getAccountInformation(UserAccount ua) throws URISyntaxException;
 
-	
+	List<CounterParty> getPayees(UserAccount ua) throws URISyntaxException;
 
 }
